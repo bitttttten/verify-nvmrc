be117b826dd958c58747fd0b2d337ff14ada127b
bitttttten <bitttttten@googlemail.com>
2021-08-17 15:31:38 +0200

    init ✨

---
+++ README.md
@@ -0,0 +1,29 @@
+# verify-nvmrc
+
+Script to help verify that the loaded node version matches what is set in the the closest .nvmrc file.
+
+## Usage
+
+You can run this script through `npx`.
+
+```sh
+npx verify-nvmrc
+# Node version is correct
+```
+
+#### Supported FLAGS
+
+- `-e`, `--emoji`
+
+Supports emojis.
+
+- `-v`, `--verbose`
+
+Logs out where it found the nvmrc file, if one is present. And logs out current node version.
+
+```sh
+npx verify-nvmrc -e -v
+# ℹ️ Current node version v14.15.4
+# ℹ️ Found .nvmrc file with v14.15.4 at /Users/bitttttten/github.com/verify-nvmrc/.nvmrc
+# Node version is correct 🎉
+```
---
+++ cli.mjs
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+import { execSync } from "child_process";
+import path from "path";
+import fs from "fs";
+
+const message = (...messages) => messages.filter(Boolean).join(" ");
+const log = (...messages) => console.log(message(...messages));
+const warn = (...messages) => console.warn(message(...messages));
+
+function runCommand(command) {
+  try {
+    return execSync(command).toString().trim();
+  } catch (e) {
+    console.error(`Failed to execute ${command}`, e);
+  }
+}
+
+async function birdUp() {
+  let directory = path.resolve();
+  const { root } = path.parse(directory);
+
+  while (true) {
+    try {
+      const lookingAt = path.resolve(directory, ".nvmrc");
+      const foundPath = await fs.promises.stat(lookingAt);
+      const contents = fs.readFileSync(lookingAt, "utf8");
+
+      if (foundPath) {
+        return {
+          path: lookingAt,
+          version: contents.startsWith("v") ? contents : `v${contents}`,
+        };
+      }
+    } catch (e) {
+      if (e.code !== "ENOENT") {
+        console.error(e);
+      }
+
+      directory = path.dirname(directory);
+
+      if (directory === root) {
+        return "unknown";
+      }
+    }
+  }
+}
+
+async function main() {
+  const args = new Set(process.argv);
+  const supportEmojis = args.has("-e") || args.has("--emoji");
+
+  const currentNodeVersion = runCommand("node --version");
+  const closestNvmrcFile = await birdUp();
+
+  if (args.has("-v") || args.has("--verbose")) {
+    log(supportEmojis && "ℹ️", `Current node version ${currentNodeVersion}`);
+    if (closestNvmrcFile !== "unknown") {
+      log(
+        supportEmojis && "ℹ️",
+        `Found .nvmrc file with ${closestNvmrcFile?.version} at ${closestNvmrcFile?.path}`
+      );
+    }
+  }
+
+  if (closestNvmrcFile === "unknown") {
+    warn(
+      supportEmojis && "🚨",
+      "Cannot find an .nvmrc file in this directory or in any it's parent directories"
+    );
+
+    return;
+  }
+
+  if (currentNodeVersion !== closestNvmrcFile?.version) {
+    warn(supportEmojis && "🚨", "Node version is NOT correct");
+
+    return;
+  }
+
+  log("Node version is correct", supportEmojis && "🎉");
+}
+
+main();
